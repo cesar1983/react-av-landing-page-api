bcf3252f47c9e43785e117951cdf9cea03b79c14
Cesar Fernandes <cesar.fa@gmail.com>
2020-08-30 20:22:45 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77110c51a2752d8bab645b75b35ed478'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8b2b388-32fc-4a8e-bcd6-b90946b87f83'
+};
